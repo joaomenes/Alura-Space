47982b21551d7fe57f36a3b09ddd09956de9dcd1
Joao <joao.menes@hotmail.com>
2024-12-18 21:42:22 -0300

    first commit

---
+++ galeria/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from galeria.models import Fotografia
+
+class ListandoFotografias(admin.ModelAdmin):
+
+    list_display = ('id', 'nome', 'legenda', 'publicado',)
+    list_display_links = ("id","nome")
+    search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable = ("publicado",)
+
+admin.site.register(Fotografia, ListandoFotografias)
