758fad1d2fb2cdcb228746d3489ba81b984322c0
Joao <joao.menes@hotmail.com>
2024-12-19 23:00:09 -0300

    Adicionando foto padrão, normalizando a busca retirando acentuações.

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
+import unicodedata
+
+def remover_acentos(texto):
+    #Remove os acentos de uma string.
+    return ''.join(
+        char for char in unicodedata.normalize('NFD', texto)
+        if unicodedata.category(char) != 'Mn'
+    )
 
 def index(request):
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicado=True)
@@ -8,3 +16,18 @@ def index(request):
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicado=True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            nome_a_buscar_sem_acento = remover_acentos(nome_a_buscar).lower()
+            #trazer os dados do banco
+            fotografias = [
+                foto for foto in fotografias
+                if remover_acentos(foto.nome.lower()).find(nome_a_buscar_sem_acento) != -1
+            ]
+
+    return render(request, "galeria/buscar.html", {"cards": fotografias})
