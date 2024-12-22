07be80649c8856fbd71c168a76e3ddb21728e785
Joao <joao.menes@hotmail.com>
2024-12-22 17:45:33 -0300

    Template cadastro e login

--- setup/urls.py
+++ setup/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
-    path('', include('usuário.urls')),
+    path('', include('usuarios.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from galeria.views import index, imagem, buscar
+from usuarios.views import login, cadastro
 
 urlpatterns = [
-    
+    path('login', login, name='login'),
+    path('cadastro', cadastro, name='cadastro'),
 ]
